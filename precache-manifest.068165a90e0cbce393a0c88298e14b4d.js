bd9fac1ca2354625f362ecd0bd4e73fb38ad5d20
TimmyGOGO <temich941@mail.ru>
2020-08-21 00:17:26 +0400

    Updates

---
+++ precache-manifest.068165a90e0cbce393a0c88298e14b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce238180e83c56052180e9d07c2ffe9f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1e6fc14a2defeeede75",
+    "url": "/monsters-rolodex/static/css/main.6087c9df.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e6fc14a2defeeede75",
+    "url": "/monsters-rolodex/static/js/main.eecedce5.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);

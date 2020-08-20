29446160f49f7160b4690f0beccdf2df50f18447
TimmyGOGO <temich941@mail.ru>
2020-08-20 23:22:11 +0400

    Added .prettierrc

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './card.styles.css';
 
-export const Card = (props) => {
+export const Card = props => {
   return (
     <div className="card-container">
-      <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`}/>
+      <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
       <h2>{props.monster.name}</h2>
       <p> {props.monster.email} </p>
     </div>
-  )
-}
-
+  );
+};
